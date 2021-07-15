b3597ba5e5ae5cdd3a7f789fe27a95e09a019288
mra437 <mariam.alexander@maersk.com>
2021-07-15 23:32:52 +0200

    initial speech recognition using puppeteer

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
+const Speech = require('./webspeech')
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -20,9 +20,23 @@ function activate(context) {
 	let disposable = vscode.commands.registerCommand('speechtocode.helloWorld', function () {
 		// The code you place here will be executed every time your command is executed
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Mariams SpeechToCode!');
-	});
+        const client = new Speech({
+            chromePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+            continuous: true,
+        })
+
+        // Display a message box to the user
+        vscode.window.showInformationMessage('Start using your microphone!');
+        
+        client.on('ready', () => {
+          client.record();
+        });
+        
+        client.on('data', (transcript) => {
+          console.log(transcript);
+        });
+
+    });
 
 	context.subscriptions.push(disposable);
 }
